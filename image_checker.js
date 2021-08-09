138ef14abd7a1fb964ba35de9fe889fc89398db4
Kasun <bgkasunnirmala@gmail.com>
2021-08-09 13:56:52 +0530

    json to txt

--- image_checker.js
+++ image_checker.js
@@ -43,7 +43,7 @@ async function connect() {
    
         });
 
-        fs.writeFileSync("images.json",(content));
+        fs.writeFileSync("images.txt",JSON.stringify(content));
  
     }catch(e){
         console.log(e);
